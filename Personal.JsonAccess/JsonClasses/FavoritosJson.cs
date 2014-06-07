cf6970fbaaa025cda7de63df2eff0c85d177243a
Ignacio <inbarreto86@gmail.com>
2014-06-06 22:26:46 -0300

    Incluidos los archivos que faltan

---
+++ Personal.JsonAccess/JsonClasses/FavoritosJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal.JsonAccess.JsonClasses
+{
+    public class FavoritosJson
+    {
+
+        public string content_id { get; set; }
+        public string session_id { get; set; }
+
+
+
+        public FavoritosJson(string content, string session)
+        {
+            this.content_id = content;
+            this.session_id = session;
+        }
+    }
+}
+
