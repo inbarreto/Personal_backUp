91314a373e40e4b3ab0bd555e2bf6c095e92effa
Ignacio <inbarreto86@gmail.com>
2014-06-07 15:14:30 -0300

    Se agregan los archivos

---
+++ Personal/Views/TerminosyCondiciones.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Personal.Views
+{
+    public partial class TerminosyCondiciones : PhoneApplicationPage
+    {
+        public TerminosyCondiciones()
+        {
+            InitializeComponent();
+        }
+    }
+}
